38a67d4bb9a9b4062b9c1fdd49cd65d68c57b466
shayanmdh86 <shayanmdhy0@gmail.com>
2023-06-07 22:00:09 +0330

    dev Add pro

--- HW5.5_ShayanMadahi_mk97/Interface/ProductRepository.cs
+++ HW5.5_ShayanMadahi_mk97/Interface/ProductRepository.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
@@ -12,9 +13,20 @@ namespace HW5._5_ShayanMadahi_mk97.Interface
     public class ProductRepository : IProductRepository
     {
         public bool Test;
+        private List<Product> products;
+        public ProductRepository()
+        {
+            string? solutionFolderPath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.FullName;
+            string solutionPath = Path.Combine(solutionFolderPath, "DataBase");
+            string FilePath = Path.Combine(solutionPath, "ProductJson.json");
+            FileStream JsonProductFile = File.Open(FilePath, FileMode.OpenOrCreate);
+
+            products = JsonSerializer.Deserialize<List<Product>>(JsonProductFile);
+        }
         public string AddProduct(Product product)
         {
-            throw new NotImplementedException();
+            CheckName(product.Name);
+            return "done";
         }
 
         public string GetProductById(int id)
