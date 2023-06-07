89cbf7224de033240eec91a4bbf7f65ec998cb48
shayanmdh86 <shayanmdhy0@gmail.com>
2023-06-07 21:27:53 +0330

    Create Domain & Services

---
+++ HW5.5_ShayanMadahi_mk97/Domain/Stock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5._5_ShayanMadahi_mk97.Domain
+{
+    public class Stock
+    {
+        public int StockId { get; set; }
+        public string Name { get; set; }
+        public int ProductId { get; set; }
+        public int ProductQuantity { get; set; }
+        public int ProductPrice { get; set; }
+    }
+}
---
+++ HW5.5_ShayanMadahi_mk97/Interface/IStockRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5._5_ShayanMadahi_mk97.Interface
+{
+    public interface IStockRepository
+    {
+
+    }
+}
