3f3a1d670860af39282d3610215d0dcbf5fe1d32
shayanmdh86 <shayanmdhy0@gmail.com>
2023-06-07 21:37:19 +0330

    Method checkname

---
+++ HW5.5_ShayanMadahi_mk97/Interface/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using HW5._5_ShayanMadahi_mk97.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HW5._5_ShayanMadahi_mk97.Interface
+{
+    public class ProductRepository : IProductRepository
+    {
+        public bool Test;
+        public string AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProductList()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool CheckName(string name) 
+        {
+            Regex regex = new Regex(@"^[A-Z][a-z]{3}[1-9]{1}[_]{1}[1-9]{2}$");
+            Test = regex.IsMatch(name);
+            if (!Test)
+                Console.WriteLine("not Correct!!");
+            return Test;
+            
+        }
+
+    }
+}
