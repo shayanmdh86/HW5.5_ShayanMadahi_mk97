245b7d91cdb31394db4725ecd6a784f8b95e665f
shayanmdh86 <shayanmdhy0@gmail.com>
2023-06-08 01:21:31 +0330

    Create custom Exception

---
+++ HW5.5_ShayanMadahi_mk97/Interface/Exception/IncorrectProductNameFormat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5._5_ShayanMadahi_mk97.Interface.Exception
+{
+    public class IncorrectProductNameFormat : ApplicationException
+    {
+        public override string Message => "The entered product name is not correct!!";
+    }
+}
