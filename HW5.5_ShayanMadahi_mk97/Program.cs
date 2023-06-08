f168389b99450b1782df6151569b0344869a1a5b
shayanmdh86 <shayanmdhy0@gmail.com>
2023-06-08 06:45:37 +0330

    test

--- HW5.5_ShayanMadahi_mk97/Program.cs
+++ HW5.5_ShayanMadahi_mk97/Program.cs
@@ -1,5 +1,6 @@
 ﻿using HW5._5_ShayanMadahi_mk97.Domain;
 using HW5._5_ShayanMadahi_mk97.Interface;
+//sing HW5._5_ShayanMadahi_mk97.Interface;
 using HW5._5_ShayanMadahi_mk97.Interface.Exception;
 
 namespace HW5._5_ShayanMadahi_mk97
@@ -8,9 +9,11 @@ namespace HW5._5_ShayanMadahi_mk97
     {
         static void Main(string[] args)
         {
-
             {
                 Product product = product();
+                ProductRepository productRepository = new ProductRepository();
+                Stock stock = new stock();
+                //define menu 
             }
         }
     }
